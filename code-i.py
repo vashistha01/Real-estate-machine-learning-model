27f9c91967c74efd1848802e3e6ab0810fa0b14c
Janvi Vashistha <128804608+vashistha01@users.noreply.github.com>
2024-01-25 16:10:54 +0530

    Create code-i.py

---
+++ code-i.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# Load the dataset
+real_estate_data = pd.read_csv("/Coding/Real_Estate.csv") #replace the path with your path
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Selecting features and target variable
+features = ['Distance to the nearest MRT station', 'Number of convenience stores', 'Latitude', 'Longitude']
+target = 'House price of unit area'
+
+X = real_estate_data[features]
+y = real_estate_data[target]
+
+# Splitting the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model initialization
+model = LinearRegression()
+
+# Training the model
+model.fit(X_train, y_train)
+
